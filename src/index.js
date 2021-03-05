597c7300f32a73b84a5501bf4771865d23d2a67d
GrigoriyNestsiarovich <79170403+GrigoriyNestsiarovich@users.noreply.github.com>
2021-03-06 01:33:51 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+ for(let i = 0; i < array.length; i++)
+    {
+       min = Math.min.apply(null,arr);
+    }
+
+return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+for(let i = 0; i < array.length; i++)
+    {
+       max = Math.max.apply(null,arr);
+    }
+
+return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+for(let i = 0; i < array.length; i++)
+    {
+        if(array.length == 0){
+            sum += arr[i];
+            avg = sum/arr.length;
+        }
+        else{
+            return 0;
+        }
+
+    }
+    return avg;
 }
