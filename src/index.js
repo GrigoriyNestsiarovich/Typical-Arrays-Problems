135d2612bdac976bb705fd38720db5e3ba1499b7
GrigoriyNestsiarovich <79170403+GrigoriyNestsiarovich@users.noreply.github.com>
2021-03-13 12:57:49 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,31 +1,35 @@
 
-exports.min = function min (array) {
-    let min = 0; 
- for(let i = 0; i < array.length; i++)
-    {
-       min = Math.min.apply(null,array);
-    }
 
-return min;
+exports.min = function min(array) {
+    let min = 0;
+    if (typeof array == 'undefined') return 0;
+    if (array.length == 0) return 0;
+    for (let i = 0; i < array.length; i++) {
+        min = Math.min.apply(null, array);
+    }
+    return min;
+
 }
 
-exports.max = function max (array) {
+exports.max = function max(array) {
     let max = 0;
-for(let i = 0; i < array.length; i++)
-    {
-       max = Math.max.apply(null,array);
+    if (typeof array == 'undefined') return 0;
+    if (array.length == 0) return 0;
+    for (let i = 0; i < array.length; i++) {
+        max = Math.max.apply(null, array);
     }
 
-return max;
+    return max;
 }
 
-exports.avg = function avg (array) {
-    let sum = 0; 
+exports.avg = function avg(array) {
+    if (typeof array == 'undefined') return 0;
+    if (array.length == 0) return 0;
+    let sum = 0;
     let avg = 0;
-    for(let i = 0; i < array.length; i++)
-        {
-            sum += array[i];
-            avg = sum/array.length;
-        }
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+        avg = sum / array.length;
+    }
     return avg;
 }
